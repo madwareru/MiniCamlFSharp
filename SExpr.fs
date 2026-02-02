1304d91adedb8f2e4de4c90c38097ef2bc4e79ad
vitalysamotokin <vitaly.samotokin@yandex.ru>
2026-02-02 12:13:52 +0300

    Удалён дублирующийся тест

--- SExpr.fs
+++ SExpr.fs
@@ -113,29 +113,3 @@ module SExpr =
             | _, garbage -> raise (ParseException $"found some garbage at the end: %A{garbage}")
 
     let parse source = source |> SExprGrammar().parse
-
-[<Test>]
-let testSExprParser () =
-    let tests: (string * SExpr.SExpr) list =
-        [ "123", SExpr.SExprInt 123
-
-          "hello?", SExpr.SExprId(Seq.toList "hello?")
-
-          "(1 2.51 #f)", SExpr.SExprList [ SExpr.SExprInt 1; SExpr.SExprFloat 2.51; SExpr.SExprBool false ]
-
-          "((1 2 3) (1 2 3) (1 2 3))",
-          SExpr.SExprList
-              [ SExpr.SExprList [ SExpr.SExprInt 1; SExpr.SExprInt 2; SExpr.SExprInt 3 ]
-                SExpr.SExprList [ SExpr.SExprInt 1; SExpr.SExprInt 2; SExpr.SExprInt 3 ]
-                SExpr.SExprList [ SExpr.SExprInt 1; SExpr.SExprInt 2; SExpr.SExprInt 3 ] ]
-
-          "(a/test 2 1 + 3)",
-          SExpr.SExprList
-              [ SExpr.SExprId(Seq.toList "a/test")
-                SExpr.SExprInt 2
-                SExpr.SExprInt 1
-                SExpr.SExprId(Seq.toList "+")
-                SExpr.SExprInt 3 ] ]
-
-    for source, expected in tests do
-        Assert.AreEqual(expected, SExpr.parse source)
