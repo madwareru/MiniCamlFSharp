31cfd4d447bdddd9623bb7bb53b42b2f195b6637
madwareru <madware.ru@gmail.com>
2026-02-04 09:04:43 +0300

    Написан модуль Elim, реализующий функционал по удалёнию неиспользуемых объявлений

--- KNorm.fs
+++ KNorm.fs
@@ -1,7 +1,9 @@
 module mini_caml_fsharp.KNorm
 
+open NUnit.Framework
 open mini_caml_fsharp.Id
 open mini_caml_fsharp.Type
+open mini_caml_fsharp.S
 
 module KNorm =
     type t =
@@ -50,3 +52,57 @@ module KNorm =
         { name: Id.t * Type.t
           args: (Id.t * Type.t) list
           body: t }
+    
+    /// Функция, определяющая набор использованых объявлений имён
+    /// в выражении. В случае если имя внешнее оно не добавляется
+    /// в наабор
+    let rec used_vars e =
+        match e with
+        | t.Unit
+        | t.Int _
+        | t.Float _
+        | t.ExtArray _ -> S.Empty ()
+        
+        | t.Neg x
+        | t.FNeg x
+        | t.Var x -> S.Singleton x
+        
+        | t.Add(x, y)
+        | t.Sub(x, y)
+        | t.FAdd(x, y)
+        | t.FSub(x, y)
+        | t.FMul(x, y)
+        | t.FDiv(x, y)
+        | t.Get(x, y) -> S.OfList [x; y]
+        
+        | t.Put(x, y, z) -> S.OfList [x; y; z]
+        
+        | t.Tuple xs
+        | t.ExtFunApply(_, xs) -> S.OfList xs
+        
+        | t.Apply(x, xs) -> S.OfList <| x::xs
+        
+        | t.BranchEq(x, y, e1, e2)
+        | t.BranchLE(x, y, e1, e2) -> (used_vars e1).Union(used_vars e2).Add(x).Add(y)
+        
+        | t.LetRec({ name = (x, _); args = yts; body = body }, cont) ->
+            // Получаем используемые в body переменные, но исключаем
+            // из них имена аргументов функции и имя функции.
+            (used_vars body)
+                .Exclude(S.OfList(yts |> List.map fst))
+                .Union(used_vars cont)
+                .Remove(x)
+        
+        | t.Let((x, _), body, cont) ->
+            // получаем имена из тела и из продолжения (убирая из
+            // набора продолжения само имя x, при этом если имело
+            // место затенение старого имени в теле, то это имя
+            // должно сохраниться. По этой причине удалние выведено
+            // внутрь скобок 
+            (used_vars body).Union((used_vars cont).Remove x)
+            
+        | t.LetTuple(xts, v, cont) ->
+            // получаем имена из продолжения, но исключаем из них
+            // все вновь объявленные, и добавляем к ним имя v
+            (used_vars cont).Exclude(S.OfList(xts |> List.map fst)).Add(v)
+                
